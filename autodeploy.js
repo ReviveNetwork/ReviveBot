c1a7667172df8cc4f9cef0513877782aea7615f9
Melroy <melroynd@live.com>
2017-01-27 14:33:59 +0400

    fixed protocol bug in autodeploy

--- autodeploy.js
+++ autodeploy.js
@@ -3,7 +3,7 @@ module.exports = {};
 var exports = module.exports;
 exports.deploy = function(){
 
- var unzip = require('unzip'); var http = require('http'); var request = http.get("https://github.com/ReviveNetwork/ReviveBot/archive/master.zip", function(response) { 
+ var unzip = require('unzip'); var http = require('http'); var request = http.get("http://github.com/ReviveNetwork/ReviveBot/archive/master.zip", function(response) { 
 response.pipe(unzip.Extract({path:'./'}))
  });
 };
