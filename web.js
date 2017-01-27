72506c9eb6f8a4e62acdf71622c0d52749e02388
meldsza <melroynd@live.com>
2017-01-27 10:57:56 +0400

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bot = require('./bot');
-const functions =  require('./functions');
+const functions =  require('./functions'); 
+const deploy = require('./autodeploy.js');
 let app = express();
 
 app.use(bodyParser.json())
@@ -13,6 +14,11 @@ app.get('/push/user/:userId/updated', function(req, res) {
 app.get('/', function(req, res) {
     res.sendFile('/html/index.html');
 });
+
+app.post('/deploy/', function(req, res) { 
+deploy.deploy();
+res.send("Completed");
+ });
 
 var listener = app.listen(process.env.PORT || 8080, function() {
     console.log('API now running on port: ' + listener.address().port);
