8af6cc1c52b75e84258ea811695b0857c3ae1d5e
Melroy <melroynd@live.com>
2017-01-28 16:21:32 +0400

    Bug fix

--- help.js
+++ help.js
@@ -1,14 +1,15 @@
 const bot = require('./bot');
 
 bot.on('message', message => {
+	var channel = message.channel;
 	message = message.content.toLowerCase();
 	if(message.includes('updating') && message.includes('forever') )
 	{
-		bot.sendMessage('Follow this article \n http://battlelog.co/wiki.php?article=27');
+		channel.sendMessage('Follow this article \n http://battlelog.co/wiki.php?article=27');
 	}
 	if(message.includes('unkown dynamicoption'))
 		{
-		bot.sendMessage('Follow the second part of this article. \n http://battlelog.co/post.php?id=9744');
+		channel.sendMessage('Follow the second part of this article. \n http://battlelog.co/post.php?id=9744');
 		}
 }
 );
