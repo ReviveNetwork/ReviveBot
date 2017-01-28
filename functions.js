0ff4b36ccaa6243f2bff5a0632a54f5ce43a104c
Spencer <spencer@sf-n.com>
2017-01-27 19:07:30 -0500

    fix name in link file

--- functions.js
+++ functions.js
@@ -29,7 +29,7 @@ exports.integrate = function(user) {
     //user is an user onject. get it using message.author
     let id = urlencode(new Buffer(JSON.stringify({
             id: user.id,
-            name: user.name
+            name: user.username
         })).toString('base64'));
     var res = "To link your discord account and forum account, please follow the below link \n https://battlelog.co/discord_link.php?id=" + id;
     user.sendMessage(res);
