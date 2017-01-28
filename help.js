6716199c8901289111226479fa7e22a2f8b99e0a
Melroy <melroynd@live.com>
2017-01-28 16:37:52 +0400

    mouse stuck solution and cdkey issues solution in help.js

--- help.js
+++ help.js
@@ -11,5 +11,12 @@ bot.on('message', message => {
 		{
 		channel.sendMessage('Follow the second part of this article. \n http://battlelog.co/post.php?id=9744');
 		}
+	if(message.includes('mouse') && message.includes('stuck'))
+		{
+		channel.sendMessage('follow this article\nhttp://battlelog.co/wiki.php?article=7');
+		}
+	if(message.includes('invalid') && message.includes('cdkey'))
+	{ channel.sendMessage('follow these articles\nhttps://battlelog.co/post.php?id=16014&page=1\nhttp://battlelog.co/wiki.php?article=28');
+	}
 }
 );
