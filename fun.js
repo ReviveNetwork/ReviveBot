a00baedf3a042c621fc0a81c1da93fe5362eee6b
meldsza <melroynd@live.com>
2017-01-29 09:58:39 +0400

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,11 +1,15 @@
 const bot = require('./bot');
+var math = require('mathjs');
 
 bot.on('message', message => {
     if (message.content.toLowerCase().startsWith('~count')) {
         c = parseInt(message.content.split(' ')[1]) || 10;
         message.channel.sendMessage('counting').then(msg => count(c, msg));
     }
-
+    if (message.content.toLowerCase().startsWith('~calc')) {
+       var res = message.content.match(/^[ 0-9\/\+\-\*]+/);
+       message.channel.sendMessage(math.eval(res));
+    }
 });
 var count = function(c, message) {
     if (c <= 0) {
