7846856f64d0fca91227a1af475f2f86581fd9e7
Spencer Sharkey <spencer@sf-n.com>
2017-01-28 22:01:58 -0500

    removed erlpack

---
+++ config.js
@@ -0,0 +1,11 @@
+
+// This is just a static export of config variables, mostly tailed for Revive Network's server
+
+//
+// WARNING:
+// do not store sensitive variables here!
+//
+
+module.exports = {
+
+}
