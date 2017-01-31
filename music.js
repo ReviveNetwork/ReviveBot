2373525388460c3cf6cf163848b0aa8908005719
Melroy <meldsza@gmail.com>
2017-01-31 15:06:42 +0400

    Music almost done

--- music.js
+++ music.js
@@ -1,3 +1,64 @@
-var bot = require("./lib/music.js");
-
-bot.run("Revive Network", "music-lobby", "Music-Lobby", "alias.json");
+var bot = require('./bot');
+module.exports = {};
+exports = module.exports;
+exports.queue = [];
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
+var playing;
+exports.play = function(URL,member)
+{
+	if(queue.size!=0)
+	{queue.push([URL,member]);return;}
+	var voiceChannel = member.voiceChannel;
+	if(voiceChannel ==undefined)
+	{
+		member.guild.channels.find(function(channel)
+		{if(channel.type==='text' && channel.name ==="music-lobby")})
+			).sendMessage("Join the Music-Lobby");
+			
+	}
+	else if(voiceChannel.name!="Music-Lobby")
+	{
+		voiceChannel = member.guild.channels.find(function(channel)
+		{
+			if(channel.type==='voice' && channel.name ==="Music-Lobby")
+		});
+		member.setVoiceChannel(voiceChannel);
+	}
+	voiceChannel.join()
+	.then(connection => {
+   const stream = ytdl(URL, {filter : 'audioonly'});
+    playing = connection.playStream(stream, streamOptions);
+	})
+	.catch(console.error);
+	
+	playing.on('end', (err) => {
+				exports.playNext();
+				});
+}
+exports.playNext = function()
+{
+	if(queue.size==0)
+	{return;}
+	var next = queue.shift();
+	exports.play(next[0],next[1]);
+}
+exports.destroy = function(message)
+{
+	if(! member.hasPermission("MOVE_MEMBERS"))
+	{message.reply("Not Worthy");return;}
+	queue = [];
+	playing.end();
+}
+exports.pause = function()
+{
+	playing.pause();
+}
+exports.resume = function()
+{
+	playing.resume();
+}
+exports.setVol(vol)
+{
+	playing.setVolume(vol);
+}
