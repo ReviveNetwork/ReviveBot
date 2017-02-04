e61566e028a263d0e05e439d4689f5df7cc9ea21
Melroy <melroynd@live.com>
2017-02-04 18:51:21 +0400

    removed index.html

--- web.js
+++ web.js
@@ -10,9 +10,6 @@ app.get('/push/user/:userId/updated', function(req, res) {
     functions.refresh(bot.users.get(req.params.userId));
 
 });
-app.get('/', function(req, res) {
-    res.sendFile('/html/index.html');
-});
 
 var listener = app.listen( 8080, function() {
     console.log('API now running on port: ' + listener.address().port);
