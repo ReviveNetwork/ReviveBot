27c8ab9443983407c6846bef157310bf34b94b79
Melroy <melroynd@live.com>
2017-02-04 15:36:28 +0400

    remove @everyone from stats

--- stats.js
+++ stats.js
@@ -13,6 +13,7 @@ let stats = (message)=>{
 	.addField('Online: ',guild.presences.map(p =>p.status!='offline').length,true)
 	.addField('Region: ',guild.region,true);
 	guild.roles.map((role) =>{
+		if(role.name !='@everyone')
 		embed.addField(role.name+": ",role.members.size,true);
 	});
 	message.channel.sendEmbed(embed);
