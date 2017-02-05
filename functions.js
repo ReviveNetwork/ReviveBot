9c92bfa60cef4379aaa4fbb141ac2e8a73e9b1ed
Melroy <melroynd@live.com>
2017-02-05 19:42:51 +0400

    dont auto refresh

--- functions.js
+++ functions.js
@@ -12,7 +12,7 @@ exports.integrate = function(user) {
         name: user.username
     })).toString('base64'));
     var res = "To link your discord account and forum account, please follow the below link \n https://battlelog.co/discord_link.php?id=" + id;
-    user.sendMessage(res);
+    //user.sendMessage(res);
     setTimeout(exports.refreshUser(user),120000);
 };
 
