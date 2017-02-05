12f4d5b0937fb233598197a8a438140d9bfdfd57
Melroy <melroynd@live.com>
2017-02-05 21:14:18 +0400

    fixed crash

--- main.js
+++ main.js
@@ -30,12 +30,14 @@ function bot_start() {
 
     bot_process.stderr.on('data', (data) => {
         console.error(data.toString());
-        bot.channels.get(config.log_channel).sendMessage('**ERROR** ReviveBot-' + bot_process.pid + ' ```' + data.subtring(0,1900)+ '```');
+	if(data.length>1900)data=data.substring(0,1900);
+        bot.channels.get(config.log_channel).sendMessage('**ERROR** ReviveBot-' + bot_process.pid + ' ```' + + '```');
     });
 
     bot_process.stdout.on('data', (data) => {
         console.log(data.toString());
-        bot.channels.get(config.log_channel).sendMessage('**LOG** ReviveBot-' + bot_process.pid + ' ```' + data.substring(0,1900) + '```');
+	if(data.length>1900)data=data.substring(0,1900);
+        bot.channels.get(config.log_channel).sendMessage('**LOG** ReviveBot-' + bot_process.pid + ' ```' +data + '```');
     });
 
     return bot_process;
