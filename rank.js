bcc31661a77dc939a617e9ef4361b7910e2048ce
Melroy <meldsza@gmail.com>
2017-02-05 10:29:39 +0400

    Changed rank img endpoints

--- rank.js
+++ rank.js
@@ -16,11 +16,11 @@ exports.rank = function(message)
 		{
 			let soldier = body.soldiers[i];
 			let gameob = soldier.game=="stella"?require('./bf2142'):require('./bf2');
-			let ranklink = soldier.game=='stella'?'http://files.2142-stats.com/2142/ranks/':'https://battlelog.co/img/ranks/rank_'
+			let ranklink = soldier.game=='stella'?'https://github.com/ReviveNetwork/ReviveBot/raw/master/img/bf2142/rank_':'https://battlelog.co/img/ranks/rank_'
 			gameob.getrank(soldier.pid).then(rank => {
 			let embed = new Discord.RichEmbed()
 			.setTitle(soldier.nickname)
-			.setThumbnail((soldier.game=='stella'?ranklink+rank+'.jpg':ranklink+rank+'.png'))
+			.setThumbnail(ranklink+rank+'.png'))
 			.addField("Game: ",(soldier.game== "stella" ? "Battlefield 2142" : "Battlefield 2"))
 			.addField("Online: ",(soldier.online == 1 ? "yes" : "no"))
 			.addField("Last Active: ",moment(soldier.last_active,"YYYY-MM-DD HH:mm:ss").fromNow())
