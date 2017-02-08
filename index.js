ea640dd315f8cd03cc660f99347cbaf08442dcf8
Melroy <meldsza@gmail.com>
2017-02-08 06:42:43 +0400

    Added MessageDB

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const web = require('./web');
 var commands = require('./commands');
 var config = require('./config');
 var functions = require('./functions');
-
+let messageDB = require('./messagedb.json');
+fs = require('fs');
+if(!messageDB.data)
+{messageDB.data = new Array();}
 bot.on('message', message => {
     if (message.author.bot == true) return; // prevent loop
     if (message.content.toLowerCase() === 'hi' || message.content.toLowerCase() === 'hello' || message.content.toLowerCase() === 'hey') {
@@ -13,18 +16,30 @@ bot.on('message', message => {
         return;
     }
     if (message.channel.id == '271350052188979201') {
-        bot.channels.get('271349742099759104').sendMessage("**" + message.author.username + ":** " + message.content);
+        bot.channels.get('271349742099759104').sendMessage("**" + message.author.username + ":** " + message.content)
+		.then(msg => messageDB.data.push({oldMessage:message.id,newMessage:msg.id}));
     } else if (message.channel.id == '271349742099759104') {
-        bot.channels.get('271350052188979201').sendMessage("**" + message.author.username + ":** " + message.content);
+        bot.channels.get('271350052188979201').sendMessage("**" + message.author.username + ":** " + message.content)
+		.then(msg => messageDB.data.push({oldMessage:message,newMessage:msg}));
     }
 });
+bot.on('messageUpdate', (oldMessage,newMessage)=>{
+	if(newMessage.channel == '271350052188979201' ||newMessage.channel == '271349742099759104')
+	{
+		messageDB.find("oldMessage",oldMessage.id).newMessage.edit(newMessage.content);
+	}
+})
 process.on('uncaughtException', function(err) {
     console.log('Caught exception: ' + err);
+	fs.writeFileSync('messagedb.json', JSON.stringify(messageDB));
     bot = require('./bot');
+});
+process.on('Exit', function(code) {
+	fs.writeFileSync('messagedb.json', JSON.stringify(messageDB));
 });
 bot.on("guildMemberAdd", (member) => {
     var user = member.user;
-    user.sendMessage("Welcome to the Revive Nwtowrk");
+    user.sendMessage("Welcome to the Revive Network");
     functions.refreshUser(user);
 });
 bot.on('message', message => {
