778dccdd5a8e6c809f05e7cfff17810d16fa33ff
meldsza <melroynd@live.com>
2017-02-19 21:10:08 +0400

    Better Coding

--- commands.js
+++ commands.js
@@ -1,25 +1,31 @@
-const bot = require('./bot');
-const functions = require('./functions');
-const music = require('./music');
-const bf2 = require('revive-stats.js').bf2;
-const bf2142 = require('revive-stats.js').bf2142;
+const bot = require('./lib/utils/bot');
+const music = require('./lib/modules/music');
 const Discord = require('discord.js')
-const rank = require('./rank');
-const request = require('request-promise');
-const stats = require('./stats');
-const math = require('mathjs');
-const crypto = require('crypto');
-const web = require('./web');
-const help = require('./help');
-const index = require('./hotline');
+const rank = require('./lib/modules/rank');
+const request = require('./lib/modules/request');
+const stats = require('./lib/modules/stats');
+const web = require('./lib/modules/web');
+const help = require('./lib/modules/help');
+const bf2 = require('./lib/modules/bf2');
+const bf2142 = require('./lib/modules/bf2142');
+const index = require('./lib/modules/hotline');
+const integrate = require('./lib/modules/link/integrate');
+const refresh = require('./lib/modules/link/refreshuser');
+const addRole = require('./lib/modules/addrole');
+const removeRole = require('./lib/modules/removerole');
+const convert = require('./lib/modules/convert');
+const hash = require('./lib/modules/hash');
 
 bot.on('message', message => {
     if (!message.content.startsWith('~')) {
         return;
     } //base case
     var cmd = message.content.split(' ')[0].substring(1).toLowerCase();
-    if (commands.hasOwnProperty(cmd))
-        commands[cmd].exec(message);
+    if (commands.hasOwnProperty(cmd)) {
+        let result = commands[cmd].exec(message);
+        if (typeof result === 'Promise')
+            result.catch(message.channel.sendMessage);
+    }
 });
 var commands = {
     'ping': {
@@ -39,88 +45,30 @@ var commands = {
     'link': {
         description: 'link revive account to this discord account',
         syntax: '~link',
-        exec: function (message) {
-            functions.integrate(message.author);
+        exec: (message) => {
+            return integrate(message.author).then(message.channel.sendMessage);
         }
     },
     'refresh': {
         description: 'refresh a user discord link status',
         syntax: '~refresh',
         exec: function (message) {
-            if (message.content.toString().includes('all')) {
-                functions.refreshAll(bot.guilds.get('184536578654339072').members.array())
-            } else if (message.mentions.users.size != 0) {
+            if (message.mentions.users.size != 0) {
                 message.mentions.users.map(functions.refreshUser);
             } else {
-
-                functions.refreshUser(message.author);
+                return functions.refreshUser(message.author).then(() => message.channel.sendMessage("Linked"));
             }
         }
     },
     'bf2': {
         description: 'finds a bf2 player',
         syntax: '~bf2 <PlayerName>',
-        exec: function (message) {
-            let nick = message.content.substring(5).trim();
-            bf2.getPlayers(nick).then(plist => {
-                console.log("BF2 in index.js gets executed");
-                if (plist == null) {
-                    message.channel.sendMessage("API Down or invalid search");
-                    return;
-                }
-                if (plist.length == 0) {
-                    message.channel.sendMessage("Player not found");
-                    return;
-                }
-				let res = "**S.no.\tName \t link**";
-                for (let i = 0; i < plist.length; i++) {
-                    if (i == 26) {
-                        res = res + "\n" + (plist.length - 25) +
-                            " more players found to Get a full list of players \n" +
-                            "<https://battlelog.co/player_search.php?q=" + nick + ">";
-                        break;
-                    } else {
-                        res = res + " \n" + (i + 1) +
-                            "\t" + "\t" + str(plist[i], 'bf2');
-                    }
-                }
-                message.channel.sendMessage(res);
-                console.log(res);
-            });
-        }
+        exec: bf2
     },
     'bf2142': {
         description: 'finds a bf2142 player',
         syntax: '~bf2142 <PlayerName>',
-        exec: function (message) {
-
-            let nick = message.content.substring(7).trim();
-            bf2142.getPlayers(nick).then(plist => {
-                console.log("BF2142 in index.js gets executed");
-                if (plist == null) {
-                    message.channel.sendMessage("API Down or invalid search");
-                    return;
-                }
-                if (plist.length == 0) {
-                    message.channel.sendMessage("Player not found");
-                    return;
-                }
-				let res = "**S.no.\tName \t link**";
-                for (let i = start; i < plist.length; i++) {
-                    if (i == 26) {
-                        res = res + "\n" + (plist.length - 25) +
-                            " more players found to Get a full list of players \n" +
-                            "<https://battlelog.co/player_search.php?q=" + nick + ">";
-                        break;
-                    } else {
-                        res = res + " \n" + (i + 1) +
-                            "\t" + "\t" + str(plist[i]);
-                    }
-                }
-                message.channel.sendMessage(res);
-                console.log(res);
-            });
-        }
+        exec: bf2142
     },
     'cookie': {
         description: 'eats a cookie',
@@ -132,30 +80,12 @@ var commands = {
     'add': {
         description: 'adds a person to a role',
         syntax: '~add <UserMention> to <roleName>',
-        exec: function (message) {
-            if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
-                message.channel.sendMessage("You aren't Worthy");
-                return;
-            }
-            var member = message.guild.member(message.mentions.users.first());
-            var msg = message.content.split("to ");
-            member.addRole(message.guild.roles.find("name", msg[msg.length - 1]));
-            message.reply('done');
-        }
+        exec: addRole
     },
     'remove': {
         description: 'removes a person to a role',
         syntax: '~remove <UserMention> from <roleName>',
-        exec: function (message) {
-            if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
-                message.channel.sendMessage("You aren't Worthy");
-                return;
-            }
-            var member = message.guild.member(message.mentions.users.first());
-            var msg = message.content.split("to ");
-            member.removeRole(message.guild.roles.find("name", msg[msg.length - 1]));
-            message.reply('done');
-        }
+        exec: removeRole
     },
     'help': {
         description: 'displays help',
@@ -166,7 +96,7 @@ var commands = {
                 embed.addField(cmd, "Description: " + commands[cmd].description + "\n" + "Syntax: " + commands[cmd].syntax);
             }
             //embed.addField('Music',"Description: Access the music bot using - as the prefix" + "\n"+ "Syntax: -help");
-            message.channel.sendEmbed(embed);
+            return message.author.sendEmbed(embed).then(() => { message.reply("See your DM") });
         }
     },
     'play': {
@@ -180,30 +110,22 @@ var commands = {
     'pause': {
         description: 'pauses the voice stream',
         syntax: '~pause',
-        exec: function (message) {
-            music.pause(message);
-        }
+        exec: music.pause
     },
     'resume': {
         description: 'resumes the voice stream',
         syntax: '~resume',
-        exec: function (message) {
-            music.resume(message);
-        }
+        exec: music.resume
     },
     'clear': {
         description: 'clears the playlist',
         syntax: '~clear',
-        exec: function (message) {
-            music.clear(message);
-        }
+        exec: music.clear
     },
     'playnext': {
         description: 'plays the next song',
         syntax: '~playnext',
-        exec: function (message) {
-            music.playNext(message);
-        }
+        exec: music.playNext
     },
     'setvol': {
         description: 'sets the play volume',
@@ -215,98 +137,31 @@ var commands = {
     'queue': {
         description: 'views the queue',
         syntax: '~queue',
-        exec: function (message) {
-            music.queue(message);
-        }
+        exec: music.queue
     },
     'info': {
         description: 'Gives the Battlelog Profile of a Verified User',
         syntax: '~info @usermention',
-        exec: function (message) {
-            rank.rank(message);
-        }
+        exec: rank
     },
     'request': {
         description: 'execute a request',
         syntax: '~request <request>',
-        exec: function (message) {
-            if (message.guild != bot.guilds.get('256299642180861953')) {
-                return;
-            }
-            var msg = message.content.substring(9).trim();
-            var call = function (error, response, body) {
-                if (error) {
-                    message.reply(error);
-                } else {
-                    console.log(body)
-                    if (body.length > 2000)
-                        body = body.toString().substring(0, 1980) + "...";
-                    message.channel.sendMessage("```Javascript\n" + body + '```');
-                }
-            };
-            if (msg.startsWith('{')) msg = JSON.parse(msg)
-            else msg = getOptions(msg);
-            request(msg, call);
-        }
+        exec: request
     },
     'stats': {
         description: 'displays the guild stats',
         syntax: '~stats',
-        exec: function (message) {
-            stats.stats(message);
-        }
+        exec: stats
     },
     'hash': {
         description: 'hashes a text.',
         syntax: '~hash <algorithm> <text>',
-        exec: function (message) {
-            let res = message.content.trim().substring(6);
-            let algo = res.split(' ')[0];
-            res = res.substring(algo.length + 1).trim();
-            if (crypto.getHashes().indexOf(algo) === -1) {
-                message.channel.sendMessage('Invalid algorithm \n Valid hashing Algorithms are:\n' + crypto.getHashes().join(' , '));
-                return;
-            }
-            res = checksum(res, algo);
-            console.log(res); message.channel.sendMessage('hash of message:' + res);
-            if (message.attachments.size > 0) {
-                res = message.attachments.first().url;
-                request(res).then(body => message.channel.sendMessage('Hash of file:\n' + checksum(body, algo)));
-            }
-        }
+        exec: hash
     },
     'convert': {
         description: 'hashes a dec number to a different base.',
-        syntax: '~convert <base> <decnumber>', exec: function (message) {
-            let res = message.content.trim().substring(9);
-            let base = res.split(' ')[0];
-            res = res.substring(base.length + 1).trim();
-            base = parseInt(base);
-            res = parseInt(res).toString(base);
-            console.log(res); message.channel.sendMessage(res);
-        }
+        syntax: '~convert <base> <decnumber>',
+        exec: convert
     }
-}
-const str = function (soldier, game) {
-    if (game == 'bf2')
-    { game = 'https://battlelog.co/bfhq.php?pid=' }
-    else
-    { game = 'https://bl2142.co/bfhq.php?pid=' }
-    return soldier.nick + '\t' + "<" + game + soldier.pid + ">";
-};
-const getOptions = function (URL) {
-    return {
-        url: URL,
-        headers: {
-            'User-Agent': 'GameSpyHTTP/1.0'
-        }
-    };
-};
-function checksum(str, algorithm) {
-    console.log(str);
-    console.log(algorithm);
-    return crypto
-        .createHash(algorithm || 'md5')
-        .update(str)
-        .digest('hex')
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var bot = require('./bot');
+var bot = require('./lib/utils/bot');
 const spawn = require('child_process').spawn;
 const commands = require('./commands');
 const config = require('./config');
@@ -16,7 +16,7 @@ function startlog() {
         console.log('pm2 logs process already started...');
         return;
     }
-	start = false;
+    start = false;
 
     pm2 = spawn('pm2', ['logs']);
     pm2.on('exit', (code, signal) => {
@@ -24,13 +24,13 @@ function startlog() {
     })
 
     pm2.stderr.on('data', (data) => {
-       // console.error(data.toString());
-        bot.channels.get(config.log_channel).sendMessage(' ```' + data.toString().substring(0,1900)+ '```');
+        // console.error(data.toString());
+        bot.channels.get(config.log_channel).sendMessage(' ```' + data.toString().substring(0, 1900) + '```');
     });
 
     pm2.stdout.on('data', (data) => {
         //console.log(data.toString());
-        bot.channels.get(config.log_channel).sendMessage( ' ```' +data.toString().substring(0,1900) + '```');
+        bot.channels.get(config.log_channel).sendMessage(' ```' + data.toString().substring(0, 1900) + '```');
     });
 
     return pm2;
@@ -42,44 +42,44 @@ function startlog() {
 */
 
 bot.on('ready', () => {
-	startlog();
+    startlog();
     bot.channels.get(config.log_channel).sendMessage('Bot Ready (pid: ' + process.pid + ')');
 })
 
 bot.on('message', message => {
-	if(message.author.bot)return;
+    if (message.author.bot) return;
     var guild = bot.guilds.get('256299642180861953');
 
     if (message.guild != guild) {
         return;
     }
-    if(! message.member.roles.find('name','dev'))return;
+    if (!message.member.roles.find('name', 'dev')) return;
     var msg = message.content.toLowerCase();
 
     if (msg == '~restart') {
-	bot.channels.get(config.log_channel).sendMessage('Exiting with code 0');
+        bot.channels.get(config.log_channel).sendMessage('Exiting with code 0');
         process.exit();
-        }
-	else if (msg == '~status') {
+    }
+    else if (msg == '~status') {
         message.reply(':) (pid: ' + process.pid + ')');
     } else if (msg.startsWith('~cmd')) {
-       const exec = require('child_process').exec;
+        const exec = require('child_process').exec;
         exec(msg.substring(5), (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            bot.channels.get(config.log_channel).sendMessage('**ERROR** Shell-' + ' ```' + error + '```\n`'+stderr+'`');
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        message.channel.sendMessage(' ```' + stdout + '```');
-        console.log(`stderr: ${stderr}`);
+            if (error) {
+                console.error(`exec error: ${error}`);
+                bot.channels.get(config.log_channel).sendMessage('**ERROR** Shell-' + ' ```' + error + '```\n`' + stderr + '`');
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            message.channel.sendMessage(' ```' + stdout + '```');
+            console.log(`stderr: ${stderr}`);
         });
     }
 });
 process.on('exit', (code) => {
-	  console.log(`About to exit with code: ${code}`);
-	});
-process.on('uncaughtException',(err) =>{
-	console.log('Uncaught Exception:'+err);
-	console.log(err.stack);
+    console.log(`About to exit with code: ${code}`);
+});
+process.on('uncaughtException', (err) => {
+    console.log('Uncaught Exception:' + err);
+    console.log(err.stack);
 });
