7080ccd910c7e8dcf62431deca07cdb4b82d212a
meldsza <melroynd@live.com>
2017-04-03 23:36:30 +0400

    add to remove

--- bot/commands/remove.js
+++ bot/commands/remove.js
@@ -11,7 +11,7 @@ async function command(params, message) {
     }
     let r = message.guild.roles.find("name", params[params.length - 1].trim());
     if(message.member.highestRole.comparePositionTo(r)<0)
-        return message.reply("You can not add this role as it is higher than your highest role");
+        return message.reply("You can not remove this role as it is higher than your highest role");
     return message.guild.member(message.mentions.users.first()).removeRole(r);
 }
 /**
