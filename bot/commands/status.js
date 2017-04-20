ca6a68a42f61eadd1105c04d2d26d2a7e542a756
meldsza <meldsza@gmail.com>
2017-04-20 21:15:21 +0530

    add status.js

---
+++ bot/commands/status.js
@@ -0,0 +1,29 @@
+
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+async function command(params, message) {
+    const exec = require('child_process').exec; 
+    exec("pm2 status", (error, stdout, stderr) => { 
+    	if (error) { 
+    		console.error(`exec error: ${error}`); 
+    		message.channel.sendCode("shell", '**ERROR** ' + error + '\n' + stderr, { split: true }); return; 
+    		} 
+    	console.log(`stdout: ${stdout}`); 
+    	message.channel.sendCode("shell", stdout + { split: true });
+    	console.log(`stderr: ${stderr}`); 
+    });
+}
+/**
+ * description of the command
+ */
+const description = "shows bot stats";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
