ea279e8ee59ad20c92de7446a40cbe2cf1164d56
meldsza <meldsza@gmail.com>
2017-04-24 22:59:15 +0400

    Remove timestamps

--- orm/Message.js
+++ orm/Message.js
@@ -2,7 +2,7 @@ const bookshelf = require('./../bookshelf');
 const bot = require('./../bot/bot');
 var Message = bookshelf.Model.extend({
     tableName: 'discord_messages',
-    hasTimestamps: true,
+    hasTimestamps: false,
     message: function () {
         return bot.guilds.get(this.channel).fetchMessage(this.messageID)
     }
