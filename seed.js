b6f6c01cd34a21728e3e3ea42c53b6999c055d40
meldsza <meldsza@gmail.com>
2017-04-24 22:57:23 +0400

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const bot = require('./bot/bot');
 const Message = require('./orm/Message');
 bot.on('ready', () => {
-    bot.channels.map((channel) => {
+    bot.channels.map(async function(channel) {
         let messages = await channel.fetchMessages({ limit: 100 })
         messages.map((message) => {
             new Message({
