0e80b51844ae7e104cdb72248202f18387b39871
meldsza <meldsza@gmail.com>
2017-04-24 23:39:12 +0400

    Update message2embed.js

--- bot/lib/message2embed.js
+++ bot/lib/message2embed.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 module.exports = (message) => {
     const embed = new Discord.RichEmbed();
-    embed.setAuthor(message.author.username)
+    embed.setAuthor(message.author.username,message.author.avatarURL)
     embed.setDescription(message.content);
     embed.setTimestamp(message.createdTimestamp)
-    embed.setThumbnail(message.author.avatarURL);
     return embed;
 }
