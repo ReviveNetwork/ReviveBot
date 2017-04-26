6277855a047ab3c9878b8c7de177f30e071e16a5
meldsza <meldsza@gmail.com>
2017-04-26 11:41:12 +0400

    add title for quote

--- bot/lib/message2embed.js
+++ bot/lib/message2embed.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 module.exports = (message) => {
     const embed = new Discord.RichEmbed();
     embed.setAuthor(message.author.username,message.author.avatarURL)
+    embed.setTitle(message.channel.name +" in "+message.guild.name)
     embed.setDescription(message.content);
     embed.setFooter(message.createdAt)
     //embed.setTitle("Quote");
