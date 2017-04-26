f9c1e72c84750309e485e57425b501e8afa1d2ed
meldsza <meldsza@gmail.com>
2017-04-26 06:34:14 +0400

    merge iga check with moderator check

--- bot/lib/refresh.js
+++ bot/lib/refresh.js
@@ -30,12 +30,8 @@ module.exports = function (user) {
             if(!member.roles.get(guild.roles.find("name", "admins").id))
             member.addRole(guild.roles.find("name", "admins")).then(user.sendMessage('Role added: admins'));
         }
-        if (info.is_mod) {
+        if (info.is_mod || (info.usergroup == 8)) {
             if(!member.roles.get(guild.roles.find("name", "moderator").id))
-            member.addRole(guild.roles.find("name", "moderator")).then(user.sendMessage('Role added: moderator'));
-        }
-        if (info.usergroup == 8) {
-            if(!member.roles.get(guild.roles.find("name", "ingame moderator").id))
             member.addRole(guild.roles.find("name", "moderator")).then(user.sendMessage('Role added: moderator'));
         }
         // member.setNickname(info.username);
