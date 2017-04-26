eea5b17c281820d7323d15a3b32725564398f7b2
meldsza <meldsza@gmail.com>
2017-04-26 17:45:37 +0400

    add support for multiqoute

--- bot/lib/message2embed.js
+++ bot/lib/message2embed.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.js');
 module.exports = (message) => {
+    let me = message.embeds.first();
     const embed = new Discord.RichEmbed();
     embed.setAuthor(message.author.username,message.author.avatarURL)
     embed.setTitle(message.channel.name +" in "+message.guild.name)
-    embed.setDescription(message.content);
+    if(!message.content =="")
+        embed.setDescription(message.content);
+    else
+        embed.setDescription("**"+me.author.name+" said in "+me.title+"**\n"+me.description+"\n *At "+me.footer+"*");
     embed.setFooter(message.createdAt)
     //embed.setTitle("Quote");
     return embed;
