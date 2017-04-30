f1cbbf8e5f41c1a399c407003818bc5d5db418f0
meldsza <meldsza@gmail.com>
2017-04-30 12:47:21 +0530

    add support for role mentions in refresh

--- bot/commands/refresh.js
+++ bot/commands/refresh.js
@@ -6,12 +6,24 @@ const refresh = require('./../lib/refresh');
  */
 async function command(params, message) {
     if (params.length >= 1)
-        return message.mentions.users.map(async function(u){
+    {
+        message.mentions.users.map(async function(u){
             if( await refresh(u) )
                 message.channel.sendMessage(u.toString() + " sucessfully linked");
             else
                 message.channel.sendMessage(u.toString() + " unable to be linked");
         });
+        message.mentions.roles.map(function(r){
+            r.members.map(async function(m){
+                let u = m.user;
+                if( await refresh(u) )
+                    message.channel.sendMessage(u.toString() + " sucessfully linked");
+                else
+                    message.channel.sendMessage(u.toString() + " unable to be linked");
+            })
+        });
+        return true;
+    }
     else {
         if(await refresh(message.author))
             return message.channel.sendMessage(message.author.toString() + " sucessfully linked")
