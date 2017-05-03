cb094f5013d1b540e7e2f1539786f03b79005eaa
meldsza <melroynd@live.com>
2017-05-03 07:08:30 +0530

    use files instead of file

--- bot/modules/hotline.js
+++ bot/modules/hotline.js
@@ -5,12 +5,12 @@ bot.on('message', message => {
     if (message.author.bot == true) return; // prevent loop
     let attach = '';
     if (message.attachments.size > 0)
-    { attach = '\n' + message.attachments.first().url; }
+    { attach = '\n' + message.attachments.first(); }
     if (message.channel.id == '271350052188979201') {
-        bot.channels.get('271349742099759104').sendEmbed(m2e(message), { file: attach })
+        bot.channels.get('271349742099759104').sendEmbed(m2e(message), { files: [attach] })
             .then(msg => messageDB.data.push({ oldMessage: message.id, newMessage: msg.id, channel: msg.channel.id }));
     } else if (message.channel.id == '271349742099759104') {
-        bot.channels.get('271350052188979201').sendEmbed(m2e(message), { file: attach })
+        bot.channels.get('271350052188979201').sendEmbed(m2e(message), { files: [attach] })
             .then(msg => messageDB.data.push({ oldMessage: message.id, newMessage: msg.id, channel: msg.channel.id }));
     }
 });
@@ -19,14 +19,14 @@ bot.on('messageUpdate', (oldMessage, newMessage) => {
     console.log("executing");
     let attach = '';
     if (newMessage.attachments.size > 0)
-    { attach = '\n' + newMessage.attachments.first().url; }
+    { attach = '\n' + newMessage.attachments.first(); }
     let m = messageDB.data.find(function (messageObj) {
         if (messageObj.oldMessage === oldMessage.id)
             return messageObj;
     });
     if (m) {
         //console.log(m);
-        bot.channels.get(m.channel).fetchMessage(m.newMessage).edit('', { file: attach, embed: m2e(newMessage) });;
+        bot.channels.get(m.channel).fetchMessage(m.newMessage).edit('', { files: [attach], embed: m2e(newMessage) });;
     }
 });
 bot.on('messageDelete', message => {
