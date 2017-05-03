a5404c02c68d55f1fc55d243b198d10be47c9f1a
meldsza <meldsza@gmail.com>
2017-05-03 17:00:02 +0530

    revert back to the old hotline

--- bot/modules/hotline.js
+++ bot/modules/hotline.js
@@ -1,16 +1,15 @@
 var bot = require('../bot');
-const m2e = require('./../lib/message2embed');
 let messageDB = { data: [] };
 bot.on('message', message => {
     if (message.author.bot == true) return; // prevent loop
     let attach = '';
     if (message.attachments.size > 0)
-    { attach = message.attachments.first().url; }
+    { attach = '\n' + message.attachments.first().url; }
     if (message.channel.id == '271350052188979201') {
-        bot.channels.get('271349742099759104').send(attach, { embed: m2e(message, true) })
+        bot.channels.get('271349742099759104').sendMessage("**" + message.author.username + ":** " + message.cleanContent + attach)
             .then(msg => messageDB.data.push({ oldMessage: message.id, newMessage: msg.id, channel: msg.channel.id }));
     } else if (message.channel.id == '271349742099759104') {
-        bot.channels.get('271350052188979201').send(attach, { embed: m2e(message, true) })
+        bot.channels.get('271350052188979201').sendMessage("**" + message.author.username + ":** " + message.cleanContent + attach)
             .then(msg => messageDB.data.push({ oldMessage: message.id, newMessage: msg.id, channel: msg.channel.id }));
     }
 });
@@ -19,14 +18,14 @@ bot.on('messageUpdate', (oldMessage, newMessage) => {
     console.log("executing");
     let attach = '';
     if (newMessage.attachments.size > 0)
-    { attach = newMessage.attachments.first().url; }
+    { attach = '\n' + newMessage.attachments.first().url; }
     let m = messageDB.data.find(function (messageObj) {
         if (messageObj.oldMessage === oldMessage.id)
             return messageObj;
     });
     if (m) {
         //console.log(m);
-        bot.channels.get(m.channel).fetchMessage(m.newMessage).edit(attach, { files: [attach], embed: m2e(newMessage, true) });;
+        bot.channels.get(m.channel).fetchMessage(m.newMessage).edit("**" + oldMessage.author.username + ":** " + newMessage.cleanContent + attach);
     }
 });
 bot.on('messageDelete', message => {
@@ -41,4 +40,3 @@ bot.on('messageDelete', message => {
         bot.channels.get(m.channel).fetchMessage(m.newMessage).then(msg => msg.delete()).then(messageDB.data.splice(messageDB.data.indexOf(m), 1)).catch(console.log);
     }
 });
-
