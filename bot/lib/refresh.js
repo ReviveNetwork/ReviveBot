a6def5e69440ee3023f4d0ad05ec9cef69a84436
meldsza <melroynd@live.com>
2017-05-06 08:21:02 +0530

    make reverse link

--- bot/lib/refresh.js
+++ bot/lib/refresh.js
@@ -1,14 +1,14 @@
 const request = require('request-promise-native');
 const bot = require('./../bot');
-module.exports = async function (user,noDM) {
+module.exports = async function (user, noDM) {
     var guild = bot.guilds.get('184536578654339072');
     var member = guild.member(user);
-    if(!member)
+    if (!member)
         return false;
     //console.log(user);
     var id = user.id;
     console.log(id);
-    return request("http://revive-bot-discord.revive.systems/v0/discord/userinfo/" + id).then(async function(body) {
+    return request("http://revive-bot-discord.revive.systems/v0/discord/userinfo/" + id).then(async function (body) {
         console.log(body);
         try { var info = JSON.parse(body); }
         catch (e) {
@@ -17,33 +17,23 @@ module.exports = async function (user,noDM) {
         if (info.hasOwnProperty('error') && !noDM) {
             user.sendMessage("To link your discord account with your battlelog account follow the link given below\n"
                 + "https://battlelog.co/discord_link.php");
-            console.log( "User " + user.username + " Not Linked. DMing user to link now");
+            console.log("User " + user.username + " Not Linked. DMing user to link now");
             return false;
         }
         await member.addRole('275317218911322112');
         //member.removeRole(guild.roles.find("name", "members"));
         if (info.is_donator) {
-            if(!member.roles.get("273105185566359562"))
-            await member.addRole(guild.roles.get("273105185566359562")).then(user.sendMessage('Role added: donators'));
+            if (!member.roles.get("273105185566359562"))
+                await member.addRole(guild.roles.get("273105185566359562")).then(user.sendMessage('Role added: donators'));
         } else {
-            if(member.roles.get("273105185566359562"))
-            await member.removeRole(guild.roles.get("273105185566359562")).then(user.sendMessage('Role removed: donators'));
+            if (member.roles.get("273105185566359562"))
+                await member.removeRole(guild.roles.get("273105185566359562")).then(user.sendMessage('Role removed: donators'));
         }
-        if (info.is_admin) {
-            if(!member.roles.get('200849956796497920'))
-            await member.addRole(guild.roles.get('200849956796497920')).then(user.sendMessage('Role added: admins'));
-        }
-        /*
-        else if(member.roles.get('200849956796497920'))
-            await member.removeRole(guild.roles.get('200849956796497920')).then(user.sendMessage('Role removed: admins'));
-        */
         if (info.is_mod || (info.usergroup == 8)) {
-            if(!member.roles.get('184676864630063104'))
+            if (!member.roles.get('184676864630063104'))
                 await member.addRole(guild.roles.get('184676864630063104')).then(user.sendMessage('Role added: moderator'));
         }
-        else if(member.roles.get('184676864630063104'))
-            await member.removeRole(guild.roles.get('184676864630063104')).then(user.sendMessage('Role removed: moderator'));
         await member.setNickname(info.username);
-        return true;
+        return info;
     });
 };
--- bot/modules/api.js
+++ bot/modules/api.js
@@ -15,11 +15,24 @@ app.get('/push/user/:userId/updated', function (req, res) {
     res.send('updated');
     res.end();
 });
-app.get('/sql/messages', function(req, res){
-  var file = path.resolve(__dirname, '..', '..','dev.sqlite3') ;
-  res.download(file); // Set disposition and send it.
+app.get('/sql/messages', function (req, res) {
+    var file = path.resolve(__dirname, '..', '..', 'dev.sqlite3');
+    res.download(file); // Set disposition and send it.
 });
-
+app.get('/reverse/:id', async function (req, res) {
+    const user = bot.users.get(req.params.id);
+    const guild = bot.guilds.get("184536578654339072");
+    const member = guild.member(user);
+    const roles = member.roles.map((r) => {
+        return r.id;
+    })
+    let result = {};
+    let r = await refreshUser(user);
+    result.name = r.username;
+    result.is_mod = roles.includes("184676864630063104");
+    result.is_admin = roles.includes("200849956796497920");
+    res.json(result);
+});
 var listener = app.listen(8080, function () {
     console.log('API now running on port: ' + listener.address().port);
 });
