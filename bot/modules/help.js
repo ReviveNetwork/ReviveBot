42ce7f1bbb300e5a1c07787f6547a6d62eed534f
meldsza <meldsza@gmail.com>
2017-05-11 11:06:08 +0530

    refine help.js

--- bot/modules/help.js
+++ bot/modules/help.js
@@ -3,18 +3,15 @@ const bot = require('../bot');
 bot.on('message', message => {
     var channel = message.channel;
     if (message.author.bot) return;
-    if (!message.channel.name) return;//it is a dm
-    if (!channel.name.contains('help')) {
-        return;
-    }
+    if (message.channel.id != "260294049964097537") return;//it is not the help channel
     var user = message.author;
     message = message.content.toLowerCase();
     //Error updating
-    if (message.includes('updating') && message.includes('forever')) {
+    if (message.includes('updat') && ( message.includes('forever') || message.includes('keeps') || message.includes('progress') )) {
         channel.send('Follow this article \n http://battlelog.co/wiki.php?article=27');
     }
     //Error debugging
-    else if (message.includes('unknown dynamicoption')) {
+    else if (message.includes('unknown') && message.includes('option')) {
         channel.send('Follow the second part of this article. \n http://battlelog.co/post.php?id=9744');
     }
     //Mouse stuck after launch ingame
@@ -22,15 +19,15 @@ bot.on('message', message => {
         channel.send('follow this article\nhttp://battlelog.co/wiki.php?article=7');
     }
     //Invalid CD key Fix
-    else if (message.includes('invalid') && message.includes('cdkey')) {
+    else if (message.includes('invalid') && message.includes('key')) {
         channel.send('follow these articles\nhttps://battlelog.co/post.php?id=16014&page=1\nhttp://battlelog.co/wiki.php?article=28');
     }
     //Installation Errors
-    else if (message.includes('installation') && message.includes('failed')) {
+    else if (message.includes('install') && message.includes('fail')) {
         channel.send('follow this article\nhttp://battlelog.co/post.php?id=17435#p57246');
     }
     //Black screen after launch
-    else if (message.includes('black screen') && message.includes('help')) {
+    else if (message.includes('black') && message.includes('screen')) {
         channel.send('Follow this article \nhttp://battlelog.co/post.php?id=9744');
     }
     //Login Errors
@@ -38,15 +35,15 @@ bot.on('message', message => {
         channel.send('Follow this article \n https://battlelog.co/post.php?id=17435#p93129');
     }
     //DirectX9 errors after launhcer
-    else if (message.includes('directx9') && message.includes('error')) {
+    else if (message.includes('direct') && message.includes('error')) {
         channel.send('Follow this article \n https://battlelog.co/wiki.php?article=14');
     }
     //Server Rules Battlelog
-    else if (message.includes('server') && message.includes('rules')) {
+    else if (message.includes('server') && message.includes('rule')) {
         channel.send('Follow this article \n https://battlelog.co/post.php?id=19581');
     }
     //Howto Report players
-    else if (message.includes('howto') && message.includes('report')) {
+    else if (message.includes('how') && message.includes('report')) {
         channel.send('Follow this article \n https://battlelog.co/post.php?id=15931');
     }
 });
