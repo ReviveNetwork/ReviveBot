f7a979044059483e029540d363a6147ce131ba0b
Melroy Neil Dsouza <meldsza@gmail.com>
2017-05-17 17:12:40 +0530

    welcome to delete command

---
+++ bot/commands/delete.js
@@ -0,0 +1,54 @@
+const bot = require('./../bot');
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+async function command(params, message) {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        message.channel.send("You aren't Worthy");
+        return;
+    }
+    let user = null;
+    let channel = message.channel;
+    let limit = 10;
+    if(message.mentions.users.first())
+    {
+        user = message.mentions.users.first();
+        params = params.filter(function(p){
+            if(!p.includes(user.id))
+                return p;
+        })
+    }
+    if(message.mentions.channels.first())
+    {
+        channel = message.mentions.channels.first();
+        params = params.filter(function(p){
+            if(!p.includes(channel.id))
+                return p;
+        })
+    }
+    if(params[0] && !isNaN(parseInt(params[0])) )
+      limit = parseInt(params[0]);
+    let messages = channel.fetchMessages({limit:limit});
+    if(user ===null)
+      return await channel.bulkDelete(messages);
+    else
+    {
+      return await channel.bulkDelete(await messages.filter(function(m){
+        if(m.author.id === user.id)
+          return m;
+      }));
+    }
+}
+/**
+ * description of the command
+ */
+const description = "delete mutltiple messages from a channel.";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
