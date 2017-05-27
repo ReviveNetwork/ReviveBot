6f4864a1b38dc849895961c583983518dd7ac15f
Melroy Neil Dsouza <meldsza@gmail.com>
2017-05-27 08:30:27 +0530

    make people accept to join

--- bot/modules/accept.js
+++ bot/modules/accept.js
@@ -1,6 +1,6 @@
 const bot = require('../bot');
 bot.on('message',async function(message){
-  if(message.channel.id === "306114389365358592")
+  if(message.channel.id === "317859245309689856")
   {
     await message.delete(10);
     if(message.content.toLowerCase().includes("accept"))
