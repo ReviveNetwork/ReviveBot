1019e7e5fe1427916ef5d7bfc4dd8e24c8a6605d
Melroy Neil Dsouza <meldsza@gmail.com>
2017-05-27 08:19:31 +0530

    make people accept to see server

--- bot/modules/accept.js
+++ bot/modules/accept.js
@@ -1,3 +1,11 @@
 const bot = require('../bot');
 bot.on('message',async function(message){
+  if(message.channel.id === "306114389365358592")
+  {
+    await message.delete(10);
+    if(message.content.toLowerCase().includes("accept"))
+    {
+      await message.member.addRole(bot.guilds.get("184536578654339072").roles.get("317854639431221248"));
+    }
+  }
 });
