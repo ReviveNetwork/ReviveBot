e74facb05bcd698ba7552e694c59f6bb8f329994
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-01 12:44:50 +0530

    welcome to exec.js (owners only)

---
+++ bot/commands/exec.js
@@ -0,0 +1,77 @@
+const settings = require('./../../settings.json');
+const request = require('request-promise-native');
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+const spawn = require('child_process').spawn;
+async function command(params, message) {
+    if (settings.owners.includes(message.author.id)) {
+        const ms = await message.channel.send("Executing: "+params.join(' '),{code:'xl'});
+        const shell = spawn(params.join(' '));
+        let outputlines =[];
+        let errorlines =[];
+        let gist;
+        const updateMessage(){
+            let out = outputlines.slice(-5);
+            let err = errorlines.slice(-5);
+            ms.edit(
+                'Command:\n```xl\n'+params.join(' ')+'```\n'
+                + 'Output:\n ```xl\n'+out.join('\n')+'```\n'
+                + (err.length>0)?('Error:\n ```xl\n'+err.join('\n')+'```\n'):""
+                + (gist)?('Gist: <'+gist+">"):""
+                   )
+        }
+        const postGist = async function(){
+            let content = '### Command:  \n```xl\n'+params.join(' ')+'```  \n'
+                + '### Output:  \n ```xl\n'+outputlines.join('\n')+'```  \n'
+                + (errorlines.length>0)?('### Error:  \n ```xl\n'+errorlines.join('\n')+'```  \n'):"";
+            const body = {
+                    "description": params.join(' '),
+                    "public": true,
+                    "files": {
+                        "output.md": {
+                            "content": content
+                        }
+                    }
+                };
+            gist = await request({
+                method: "POST",
+                body: body,
+                uri: "https://api.github.com/gists",
+                headers: {
+                    'Content-Type':'application/json'
+                }
+            });
+            gist = JSON.parse(gist);
+            gist = gist.url;
+        }
+        shell.stdout.on('data', function (data) {
+            outputlines.push(data.toString());
+            updateMessage();
+        });
+        shell.stderr.on('data', function (data) {
+            errorlines.push(data.toString());
+            updateMessage();
+        });
+        shell.on('exit', async function (code) {
+            outputlines.push("Exited with code: "+code);
+            await postGist();
+            updateMessage();
+        });
+    }
+    else
+        message.reply('Not worthy')
+}
+/**
+ * description of the command
+ */
+const description = "executes a command (owners only)";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
