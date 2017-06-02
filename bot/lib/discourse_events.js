d0395c10d12a8f47d631069da5062c8ce1a36dde
meldsza <melroynd@live.com>
2017-06-02 10:14:33 +0530

    debugging

--- bot/lib/discourse_events.js
+++ bot/lib/discourse_events.js
@@ -1,9 +1,10 @@
 const request = require('request-promise-native');
 const bot = require('./../bot');
-//const md = require('html-md');
+const md = require('to-markdown');
 const RichEmbed = require('discord.js').RichEmbed;
 const did_from_uname = async function (uname) {
     let res = await request('http://localhost/v0/discord/did_from_uname/' + encodeURIComponent(uname))
+    console.log(res)
     return res.id;
 }
 module.exports = {
@@ -13,11 +14,11 @@ module.exports = {
     'post': async function (body) {
         console.log("Recieved a post event for post no: " + body.post.post_number);
         let embed = new RichEmbed();
-        embed.setAuthor(body.post.username, body.post.avatar_template.replace('{size}', "100")).setDescription(body.post.cooked)
+        embed.setAuthor(body.post.username, body.post.avatar_template.replace('{size}', "100")).setDescription(md(body.post.cooked))
         if (body.post.reply_to_user && body.post.reply_to_user.username) {
             let id = await did_from_uname(body.post.reply_to_user.username);
-            if(id)
-                bot.users.get(id).send('You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number,{embed: embed})
+            if (id)
+                bot.users.get(id).send('You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number, { embed: embed })
         }
     },
     'user': async function (body) {
