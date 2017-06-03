5764f3012c9eeaed732a7743142bb3d2f4ff7b07
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-03 23:02:05 +0530

    Add DM event

--- bot/lib/discourse_events.js
+++ bot/lib/discourse_events.js
@@ -2,6 +2,7 @@ const request = require('request-promise-native');
 const bot = require('./../bot');
 const md = require('to-markdown');
 const RichEmbed = require('discord.js').RichEmbed;
+const request = require('request-promise-native');
 const mentionReg = /\[@[a-z]*\]\(\/u\/[a-z]*\)/igm;
 const did_from_uname = async function (uname) {
     let res = await request('http://localhost/v0/discord/did_from_uname/' + encodeURIComponent(uname));
@@ -20,20 +21,39 @@ module.exports = {
         body.post.cooked = md(body.post.cooked)
         embed.setAuthor(body.post.username, body.base_url + body.post.avatar_template.replace('{size}', "100")).setDescription(body.post.cooked)
         if (body.post.reply_to_user && body.post.reply_to_user.username) {
-            toMention.push(body.post.reply_to_user.username);
+            toMention.push({
+                name: body.post.reply_to_user.username,
+                message: 'You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number
+            });
         }
         let mentions = body.post.cooked.match(mentionReg);
         if(mentions && mentions!=null)
         {
             mentions.map(function(m){
                 let u = m.match(/@[a-z]*/i)[0].substring(1);
-                return toMention.push(u)
+                toMention.push({
+                    name: u,
+                    message: 'You were mentioned by ' + body.post.username + ' in ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number
+                });
             });
+        }
+        // check for private_message
+        let topic = await request(body.base_url+ '/t/'+body.post.topic_id+'.json');
+        topic = JSON.parse(topic);
+        if(topic.archetype && topic.archetype ==='private_message' && topic.details.participants)
+        {
+            topic.details.participants.map(function(p){
+                if(p.username === body.post.username)
+                    toMention.push({
+                    name: p.username,
+                    message: 'You were DMed by ' + body.post.username + ' in ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number
+                });
+            })
         }
         for(let i=0;i<toMention.length;i++)
         {
-            let id = await did_from_uname(toMention[i]);
-            bot.users.get(id).send('You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number, { embed: embed })
+            let id = await did_from_uname(toMention[i].name);
+            bot.users.get(id).send(toMention[i].message, { embed: embed })
         }
     },
     'user': async function (body) {
