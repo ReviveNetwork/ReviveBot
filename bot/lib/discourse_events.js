48792170d1159e387ea83dbf1e25dbb459626c83
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-03 16:22:42 +0530

    notify users who are mentioned too

--- bot/lib/discourse_events.js
+++ bot/lib/discourse_events.js
@@ -2,6 +2,7 @@ const request = require('request-promise-native');
 const bot = require('./../bot');
 const md = require('to-markdown');
 const RichEmbed = require('discord.js').RichEmbed;
+const mentionReg = /\[@[a-z]*\]\(\/u\/[a-z]*\)/igm;
 const did_from_uname = async function (uname) {
     let res = await request('http://localhost/v0/discord/did_from_uname/' + encodeURIComponent(uname));
     console.log(res);
@@ -14,12 +15,20 @@ module.exports = {
     },
     'post': async function (body) {
         console.log("Recieved a post event for post no: " + body.post.post_number);
+        let toMention =[];
         let embed = new RichEmbed();
         embed.setAuthor(body.post.username, body.base_url + body.post.avatar_template.replace('{size}', "100")).setDescription(md(body.post.cooked))
         if (body.post.reply_to_user && body.post.reply_to_user.username) {
-            let id = await did_from_uname(body.post.reply_to_user.username);
-            if (id)
-                bot.users.get(id).send('You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number, { embed: embed })
+            toMention.push(body.post.reply_to_user.username);
+        }
+        body.cooked.match(mentionReg).map(function(m){
+            let u = m.match('/@[a-z]*/i')[0].substring(1);
+            return toMention.push(u)
+        })
+        for(let i=0;i<toMention.length;i++)
+        {
+            let id = await did_from_uname(toMention[i]);
+            bot.users.get(id).send('You recieved a reply from ' + body.post.username + ' for post ' + body.base_url + '/t/' + body.post.topic_slug + '/' + body.post.topic_id + '/' + body.post.post_number, { embed: embed })
         }
     },
     'user': async function (body) {
