eb7de23b5ab6b10709104e64bddcfe46b8fe6b2c
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-06 23:45:25 +0530

    use process.env.DISCOURSE_API

--- bot/lib/discourse_events.js
+++ bot/lib/discourse_events.js
@@ -1,6 +1,5 @@
 const request = require('request-promise-native');
 const bot = require('./../bot');
-const settings = require('./../../settings');
 const md = require('to-markdown');
 const RichEmbed = require('discord.js').RichEmbed;
 const mentionReg = /\[@[a-z]*\]\(\/u\/[a-z]*\)/igm;
@@ -38,7 +37,7 @@ module.exports = {
             });
         }
         // check for private_message
-        let topic = await request(body.base_url+ '/t/'+body.post.topic_id+'.json?api_key='+settings.discourse_api+'&api_username=revivebot');
+        let topic = await request(body.base_url+ '/t/'+body.post.topic_id+'.json?api_key='+ process.env.DISCOURSE_API +'&api_username=revivebot');
         topic = JSON.parse(topic);
         if(topic.archetype && topic.archetype ==='private_message' && topic.details.participants)
         {
