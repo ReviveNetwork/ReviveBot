31dc8ad900eed2b25ebbdd4e9b03c9cb1cfd49c7
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-06 08:43:33 +0530

    Add exception to autodelete in #welcome

--- bot/modules/accept.js
+++ bot/modules/accept.js
@@ -2,7 +2,8 @@ const bot = require('../bot');
 bot.on('message',async function(message){
   if(message.channel.id === "317859245309689856")
   {
-    await message.delete(10);
+    if(message.member.roles.get("317854639431221248"))
+       await message.delete(10);
     if(message.content.toLowerCase().includes("accept"))
     {
       await message.member.removeRole(bot.guilds.get("184536578654339072").roles.get("317854639431221248"));
