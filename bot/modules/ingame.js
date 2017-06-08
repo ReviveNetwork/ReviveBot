bb6bccc3b8c2d2fda6e0cda2629e89a98793f850
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-08 22:28:07 +0530

    refresh online every 15 seconds

--- bot/modules/ingame.js
+++ bot/modules/ingame.js
@@ -1,5 +1,7 @@
 const bot = require('./../bot');
-bot.on('ready',async function(){
+let re = false;
+const request = require('request-promise-native');
+bot.on('refreshOnline',async function(){
   const guild =  bot.guilds.get("184536578654339072");
   const ingame = guild.roles.get("322233107489226764");
   let playing = guild.members.filterArray(function (m) {
@@ -17,7 +19,22 @@ bot.on('ready',async function(){
    playing.map(async function(m){
       await m.addRole(ingame);
    });
+  let online = await request('http://localhost/v0/discord/online');
+  online = JSON.parse(online);
+  online.map(async function(u){
+    let user = bot.users.get(u);
+    let member = guild.member(user);
+    if(member)
+       await member.addRole(ingame);
+  })
 });
+bot.on('ready',()=>{
+  if(!re)
+    re = setInterval(function(){
+        bot.emit('refreshOnline')
+      },15000)
+})
+/**
 bot.on('presenceUpdate',async function(om,m){
     if (om.presence.game && om.presence.game.name)
             if (om.presence.game.name.toLowerCase().includes("battlefield 2"))
@@ -26,3 +43,4 @@ bot.on('presenceUpdate',async function(om,m){
             if (m.presence.game.name.toLowerCase().includes("battlefield 2"))
                 await m.addRole("322233107489226764");
 })
+*/
