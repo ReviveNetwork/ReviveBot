5d2a9aa4a967327f33cf336b13c195be2c1b4ad8
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-11 19:24:46 +0530

    update guild obj with full cache

--- bot/modules/ingame.js
+++ bot/modules/ingame.js
@@ -9,8 +9,7 @@ const updateIngame = async function(){
   let playing = await request('http://localhost/v0/discord/online');
   playing = JSON.parse(playing);
   */
-  let members = await guild.fetchMembers();
-  console.log(typeof members)
+  guild = await guild.fetchMembers();
   let playing = guild.members.filterArray(function (m) {
         if (m.presence.game && m.presence.game.name)
             if (m.presence.game.name.toLowerCase().includes("battlefield 2"))
