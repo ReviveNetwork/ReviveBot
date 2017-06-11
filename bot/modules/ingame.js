62db8f20978105d7cf997fcf240bbfcd8218fe4d
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-11 18:46:44 +0530

    revert back to using discord ingame check

--- bot/modules/ingame.js
+++ bot/modules/ingame.js
@@ -5,8 +5,15 @@ let ingame;
 let guild;
 const updateIngame = async function(){
   if(!guild)return;
+  /*
   let playing = await request('http://localhost/v0/discord/online');
   playing = JSON.parse(playing);
+  */
+  let playing = guild.members.filterArray(function (m) {
+        if (m.presence.game && m.presence.game.name)
+            if (m.presence.game.name.toLowerCase().includes("battlefield 2"))
+                return m.user.id;
+    });
   let toRemove = ingame.members.filter(function (m){
       if(!playing.includes(m.id))
         return m;
