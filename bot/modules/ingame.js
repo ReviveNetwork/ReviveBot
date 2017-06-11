52708e79a40c8be26abf9b030d0ba669de54e9b9
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-11 19:14:09 +0530

    use full collection

--- bot/modules/ingame.js
+++ bot/modules/ingame.js
@@ -9,7 +9,8 @@ const updateIngame = async function(){
   let playing = await request('http://localhost/v0/discord/online');
   playing = JSON.parse(playing);
   */
-  let playing = guild.members.filterArray(function (m) {
+  let members = await guild.fetchMembers();
+  let playing = members.filterArray(function (m) {
         if (m.presence.game && m.presence.game.name)
             if (m.presence.game.name.toLowerCase().includes("battlefield 2"))
                 return m.user.id;
