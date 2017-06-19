7330d04f5a745bf19095d76c16e85ea99ba2f2b4
Melroy Neil Dsouza <meldsza@gmail.com>
2017-06-19 21:36:03 +0530

    Create unmute.js

---
+++ bot/commands/unmute.js
@@ -0,0 +1,35 @@
+
+/**
+ * This method should return the response directly to the channel
+ * @param {*string array} params 
+ * @param {*message} message
+ */
+const settings = require('./../../settings.json');
+async function command(params, message) {
+    if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) {
+        message.channel.send("You aren't Worthy");
+        return;
+    }
+    let mutei = settings.muted.findIndex(function(m){
+      if(m.id === message.mentions.first() && m.guild === message.guild.id)
+        return true;
+    });
+    if(mutei && mutei>=0)
+    {
+      settings.muted.splice(mutei, 1);
+      message.reply("unmuted");
+    }
+    else 
+      message.reply("user was not muted");
+}
+/**
+ * description of the command
+ */
+const description = "Adds a role to a user. \nSyntax: add <mention> to role";
+/**
+ * Define Exports
+ */
+module.exports = {
+    execute: command,
+    description: description
+};
