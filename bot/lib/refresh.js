bcdc0ba8beac8328b6bc610e650753310e48e6f0
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-05 12:01:38 +0530

    better refresh

--- bot/lib/refresh.js
+++ bot/lib/refresh.js
@@ -8,53 +8,50 @@ module.exports = async function (user, noDM) {
     //console.log(user);
     var id = user.id;
     console.log(id);
-    return request("http://localhost/v0/discord/userinfo/" + id).then(async function (body) {
-        console.log(body);
-        try { var info = JSON.parse(body); }
-        catch (e) {
-            throw e;
-        }
-        if (info.hasOwnProperty('error')) {
-            if(!noDM)
-            {
-                user.send("To link your discord account with your battlelog account follow the link given below\n"
+    let body = await request("http://localhost/v0/discord/userinfo/" + id)
+    console.log(body);
+    try { var info = JSON.parse(body); }
+    catch (e) {
+        throw "API Error";
+    }
+    if (info.hasOwnProperty('error')) 
+    {
+        if(!noDM)
+        {
+            user.send("To link your discord account with your battlelog account follow the link given below\n"
                 + "https://battlelog.co/discord_link.php");
                 console.log("User " + user.username + " Not Linked. DMing user to link now");
-            }
-            await member.removeRole('275317218911322112');
-            return false;
         }
-        if(member.roles.get("317854639431221248"))
-            await member.removeRole(bot.guilds.get("184536578654339072").roles.get("317854639431221248"));
-        await member.addRole('275317218911322112');
-        //member.removeRole(guild.roles.find("name", "members"));
-        if (info.is_donator) {
-            if (!member.roles.get("273105185566359562"))
-                await member.addRole(guild.roles.get("273105185566359562")).then(user.send('Role added: donators'));
-        } else {
-            if (member.roles.get("273105185566359562"))
-                await member.removeRole(guild.roles.get("273105185566359562")).then(user.send('Role removed: donators'));
+        await member.removeRole('275317218911322112');
+        return false;
+    }
+    await member.addRole('275317218911322112');
+    if (info.is_donator) {
+        if (!member.roles.get("273105185566359562"))
+            await member.addRole(guild.roles.get("273105185566359562")).then(user.send('Role added: donators'));
+    } else {
+        if (member.roles.get("273105185566359562"))
+            await member.removeRole(guild.roles.get("273105185566359562")).then(user.send('Role removed: donators'));
         }
-        if(info.banned == 1)
-        {
-            info.username = "[BANNED] "+info.username;
-            if (!member.roles.get("318990525539680257"))
-                await member.addRole(guild.roles.get("318990525539680257")).then(user.send('Role added: BANNED'));
-        }
-        else
-        {
-            if (member.roles.get("318990525539680257"))
-                await member.removeRole(guild.roles.get("318990525539680257")).then(user.send('Role removed: BANNED'));   
-        }
-        if (info.is_donator) {
-            if (!member.roles.get("273105185566359562"))
-                await member.addRole(guild.roles.get("273105185566359562")).then(user.send('Role added: donators'));
-        } else {
-            if (member.roles.get("273105185566359562"))
-                await member.removeRole(guild.roles.get("273105185566359562")).then(user.send('Role removed: donators'));
-        }
-        if (member.kickable)
-            await member.setNickname(info.username);
-        return info;
-    });
+    if(info.banned == 1)
+    {
+        info.username = "[BANNED] "+info.username;
+        if (!member.roles.get("318990525539680257"))
+            await member.addRole(guild.roles.get("318990525539680257")).then(user.send('Role added: BANNED'));
+    }
+    else
+    {
+        if (member.roles.get("318990525539680257"))
+            await member.removeRole(guild.roles.get("318990525539680257")).then(user.send('Role removed: BANNED'));   
+    }
+    if (info.is_donator) {
+        if (!member.roles.get("273105185566359562"))
+            await member.addRole(guild.roles.get("273105185566359562")).then(user.send('Role added: donators'));
+    } else {
+        if (member.roles.get("273105185566359562"))
+            await member.removeRole(guild.roles.get("273105185566359562")).then(user.send('Role removed: donators'));
+    }
+    if (member.kickable)
+        await member.setNickname(info.username);
+    return info;
 };
