8fffb99307261fa2d0be4a5763f4fca2d652ba8c
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-08 19:48:18 +0530

    Disable status temp

--- bot/commands/status.js
+++ bot/commands/status.js
@@ -5,16 +5,7 @@
  * @param {*message} message
  */
 async function command(params, message) {
-	const exec = require('child_process').exec;
-	exec("pm2 status", (error, stdout, stderr) => {
-		if (error) {
-			console.error(`exec error: ${error}`);
-			message.channel.send('**ERROR** ' + error + '\n' + stderr, { split: true, code: 'shell' }); return;
-		}
-		console.log(`stdout: ${stdout}`);
-		message.channel.send(stdout, { split: true, code: 'shell' });
-		console.log(`stderr: ${stderr}`);
-	});
+	
 }
 /**
  * description of the command
