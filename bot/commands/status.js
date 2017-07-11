29ecc51080e7b9aae6d93f2daa26f38f78baa6f0
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-11 18:16:32 +0530

    Update status.js

--- bot/commands/status.js
+++ bot/commands/status.js
@@ -6,6 +6,8 @@
  */
 const pm2 = require('pm2');
 const moment = require('moment');
+const settings = require('./../../settings.json');
+
 async function command(params, message) {
 	if (!settings.owners.includes(message.author.id)) return await message.reply("You are not allowed to use this command");
 	const embed = new Discord.RichEmbed();
