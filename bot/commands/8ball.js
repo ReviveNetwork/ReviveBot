4aa82efb200ffae1cb4a260385b4f938e5fb7d28
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-13 17:17:21 +0530

    add some spice to 8ball

--- bot/commands/8ball.js
+++ bot/commands/8ball.js
@@ -6,6 +6,10 @@ const rp = require('request-promise-native');
  */
 async function command(params, message) {
     let url = "https://yesno.wtf/api/";
+    if(message.content.toLowerCase().contains("what") || message.content.toLowerCase().contains("why"))
+        url = url +"?force=maybe";
+    if(message.content.toLowerCase().contains("did") || message.content.toLowerCase().contains("you"))
+        url = url +"?force=maybe";
     const r = JSON.parse(await rp(url));
 
     return await message.channel.send(r.answer, { files: [r.image] });
