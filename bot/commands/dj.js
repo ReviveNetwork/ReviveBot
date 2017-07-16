46586116fa24b849e0b8dcf2cf2d0b57059c635c
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-16 10:03:05 +0530

    nerf DJ command

--- bot/commands/dj.js
+++ bot/commands/dj.js
@@ -7,19 +7,21 @@
 async function command(params, message) {
     if (!message.guild.roles.find("name", "DJ"))
         return message.reply("Sorry, this command is not applicable in this guild");
+    if (!message.member.roles.find((r)=>r.name.toLowerCase().includes("member")))
+        return message.reply("Sorry, you should be a verified member of this guild");
     if ((Date.now() - message.author.joinedTimestamp) < 2.628e+9)
         message.reply("You have to be a part of this guild for more than 1 month to earn this role");
     let m = await message.channel.send(message.author.toString() + " has applied to be a DJ. Please vote by clicking the below reactions");
     await m.react("✅");
     await m.react("⛔");
-    let r = await m.awaitReactions(r => true, { maxUsers:6 ,max: 6, time: 3.6e+6 });
+    let r = await m.awaitReactions(r => true, { maxUsers:10 ,max: 10, time: 3.6e+6 });
     console.log(r);
     if(!r.get("✅") || !r.get("⛔")) 
         return await message.reply("Error, votes could not be found");
     let yes = r.get( "✅").users.size;
     let no = r.get("⛔").users.size;
     await m.clearReactions();
-    if (yes > no && (yes + no > 4)) {
+    if (yes > no && (yes + no > 6)) {
         await m.edit(message.author.toString() + " is now a DJ");
         await message.member.addRole(message.guild.roles.find("name", "DJ"));
     }
