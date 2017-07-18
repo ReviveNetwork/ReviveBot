7d93a4e6930508d4c5db0293f16cb131f8d787e0
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-18 21:03:11 +0530

    reduce to 1700

--- bot/listen.js
+++ bot/listen.js
@@ -46,7 +46,7 @@ bot.on('message', async function (message) {
     if (message.author.bot) return;
     if (settings.slowmov && message.channel.deletable && message.member.bannable)
         setTimeout(() => {
-            if (message.member.lastMessage && message.member.lastMessage.createdTimestamp > Date.now()- 2200) {
+            if (message.member.lastMessage && message.member.lastMessage.createdTimestamp > Date.now()- 1700) {
                 message.channel.overwritePermissions(message.author, { 'SEND_MESSAGES': false }, "Muted");
                 message.reply("Calm down")
 
