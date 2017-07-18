11587c26ca23d724dca501ed9edf3ef0440e4a49
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-18 21:28:27 +0530

    Fix

--- bot/listen.js
+++ bot/listen.js
@@ -46,7 +46,7 @@ bot.on('message', async function (message) {
     if (message.author.bot) return;
     if (settings.slowmov && message.channel.deletable && message.member.bannable)
         setTimeout(() => {
-            if (message.member.lastMessage && message.member.lastMessage.createdTimestamp > Date.now()- 2400) {
+            if (message.member.lastMessage && message.member.lastMessage.createdTimestamp > Date.now()- 1700) {
                 message.channel.overwritePermissions(message.author, { 'SEND_MESSAGES': false }, "Muted");
                 message.reply("Calm down")
 
@@ -56,7 +56,7 @@ bot.on('message', async function (message) {
                         p.delete();
                 }, 5000)
             }
-        }, 2000)
+        }, 1800)
 
     if (message.content.startsWith(settings.identifier)) {
         /**Extracting params */
