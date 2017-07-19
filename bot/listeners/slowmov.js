59cb89419ec2ba21305e8ae768c9b9844a675cd5
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-19 19:59:30 +0530

    tweak mute

--- bot/listeners/slowmov.js
+++ bot/listeners/slowmov.js
@@ -1,13 +1,20 @@
 const settings = require('./../../settings.json');
 module.exports = message => {
-    if (message.member && message.member != null) {
-        let muted = message.member.roles.find(function (r) {
-            if (r.name.toLowerCase().includes('mute') && !r.name.toLowerCase().includes('non') && !r.name.toLowerCase().includes('test')) return r
-        })
-        if (muted && message.deletable)
-            message.delete();
+    if(message.author.bot) return;//ignore if its a verified bot account
+    
+    if(!message.guild) return;//ignore if DM
+    
+    let muted = message.member.roles.find(function (r) {
+        if (r.name.toLowerCase().includes('mute') && !r.name.toLowerCase().includes('non') && !r.name.toLowerCase().includes('test'))
+            return r
+    })
+    if (muted && message.deletable)
+    {
+        message.delete();
+        message.channel.overwritePermissions(muted, { 'SEND_MESSAGES': false }, "Muted").catch(() => console.log("i tried my best"));
     }
-    if (!message.author.bot && settings.slowmov && message.channel.deletable && !message.member.permissions.has("MANAGE_MESSAGES"))
+    
+    if (settings.slowmov && message.channel.deletable && !message.member.permissions.has("MANAGE_MESSAGES"))
         setTimeout(async function () {
             if (message.member.lastMessage && message.member.lastMessage.id != message.id) {
                 let duration = 10000;
