478de3b3922245fe00686d2bd72757ee656a3eac
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-20 20:10:52 +0530

    Update access_log.js

--- bot/commands/access_log.js
+++ bot/commands/access_log.js
@@ -25,7 +25,7 @@ async function command(params, message) {
         else {
             let res = `${user.toString()} has used ${logs_for_user.length} since ${moment(access_log[0].time).fromNow()}`;
             logs_for_user.map((l) => {
-                res = res + "\n" + moment(l.time) + " - ~" + l.command + " " + ((l.sucess) ? "Sucess" : "Failed") + ((l.error) ? " because of error" : "") + ((l.channel) ? ` - <#${l.channel}>` : "")
+                res = res + "\n" + moment(l.time) + " - ~" + l.command + " " + ((l.success) ? "Success" : "Failed") + ((l.error) ? " because of error" : "") + ((l.channel) ? ` - <#${l.channel}>` : "")
             })
             await message.channel.send(res, { split: true, code: 'xl' });
         }
