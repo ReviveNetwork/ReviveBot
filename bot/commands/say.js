a32415bea97293fe95844ae90a587a45312bc308
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-21 10:01:46 +0530

    Update say.js

--- bot/commands/say.js
+++ bot/commands/say.js
@@ -10,7 +10,6 @@ async function command(params, message) {
         await message.reply('https://media.tenor.com/images/c472d1ee8c75a50f700bd028cc1b10b9/tenor.gif')
         return false;
     }
-    await message.guild.me.setNickname(user.username);
     if (params.length < 1)
         await message.channel.send("No argument provided", { code: 'error' });
     else
